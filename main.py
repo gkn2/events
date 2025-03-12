f16e8ddaca06384d90f5756a04fe973f617be3cc
gkn2 <112503427+gkn2@users.noreply.github.com>
2025-03-12 11:41:29 -0400

    Update pxt.json, main.ts, main.py, images.g.jres, images.g.ts

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 class SpriteKind:
     z = SpriteKind.create()
     skeleton = SpriteKind.create()
+    ms2 = SpriteKind.create()
 
 
 
@@ -29,7 +30,7 @@ sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_overlap)
 enemy = sprites.create(assets.image("""enemy"""), SpriteKind.enemy)
 
 enemy.set_position(randint(0,160), randint(0,120))
-enemy.follow(my_sprite, 50)
+enemy.follow(my_sprite, 20)
 
 def on_overlap2(da_enemy, da_player):
     info.change_life_by(-1)
@@ -39,8 +40,39 @@ sprites.on_overlap(SpriteKind.enemy, SpriteKind.player, on_overlap2)
 
 spr = sprites.create(assets.image("""plate"""), SpriteKind.z)
 spr.set_position(randint(0,160), randint(0,120))
+spr.set_scale(.5, ScaleAnchor.MIDDLE)
 
 def on_overlap3(sprite, otherSprite):
     info.change_life_by(1)
     sprites.destroy(otherSprite)
+    enemy = sprites.create(assets.image("""enemy"""), SpriteKind.enemy)
+    enemy.set_position(randint(0,160), randint(0,120))
+    enemy.follow(my_sprite, 20)
 sprites.on_overlap(SpriteKind.player, SpriteKind.z, on_overlap3)
+
+def on_event_pressed2():
+    h2 = sprites.create(assets.image("""h2"""), SpriteKind.ms2)
+    h2.set_position(randint(0, 160), randint(0, 120))
+    h2.follow(enemy, 25)
+    h2.set_stay_in_screen(True)
+    h2.set_bounce_on_wall(True)
+    info.change_score_by(-1)
+controller.B.on_event(ControllerButtonEvent.PRESSED, on_event_pressed2)
+
+def on_overlap4(sprite, otherSprite):
+    sprites.destroy(sprite)
+    sprites.destroy(otherSprite)
+sprites.on_overlap(SpriteKind.ms2, SpriteKind.enemy, on_overlap4)
+
+def on_update_interval():
+    enemy = sprites.create(assets.image("""enemy"""), SpriteKind.enemy)
+    enemy.set_position(randint(0,160), randint(0,120))
+    enemy.follow(my_sprite, 20)
+game.on_update_interval(10000, on_update_interval)
+
+def on_update_interval2():
+    spr = sprites.create(assets.image("""plate"""), SpriteKind.z)
+    spr.set_position(randint(0,160), randint(0,120))
+    spr.set_scale(.5, ScaleAnchor.MIDDLE) 
+game.on_update_interval(5000, on_update_interval2)
+
--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 namespace SpriteKind {
     export const z = SpriteKind.create()
     export const skeleton = SpriteKind.create()
+    export const ms2 = SpriteKind.create()
 }
 
 function say_something(sprite: Sprite) {
@@ -20,7 +21,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function on_overlap(sprite
 })
 let enemy = sprites.create(assets.image`enemy`, SpriteKind.Enemy)
 enemy.setPosition(randint(0, 160), randint(0, 120))
-enemy.follow(my_sprite, 50)
+enemy.follow(my_sprite, 20)
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function on_overlap2(da_enemy: Sprite, da_player: Sprite) {
     info.changeLifeBy(-1)
     da_enemy.setPosition(randint(0, 160), randint(0, 120))
@@ -28,7 +29,33 @@ sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function on_overlap2(da_e
 })
 let spr = sprites.create(assets.image`plate`, SpriteKind.z)
 spr.setPosition(randint(0, 160), randint(0, 120))
+spr.setScale(.5, ScaleAnchor.Middle)
 sprites.onOverlap(SpriteKind.Player, SpriteKind.z, function on_overlap3(sprite: Sprite, otherSprite: Sprite) {
     info.changeLifeBy(1)
     sprites.destroy(otherSprite)
+    let enemy = sprites.create(assets.image`enemy`, SpriteKind.Enemy)
+    enemy.setPosition(randint(0, 160), randint(0, 120))
+    enemy.follow(my_sprite, 20)
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function on_event_pressed2() {
+    let h2 = sprites.create(assets.image`h2`, SpriteKind.ms2)
+    h2.setPosition(randint(0, 160), randint(0, 120))
+    h2.follow(enemy, 25)
+    h2.setStayInScreen(true)
+    h2.setBounceOnWall(true)
+    info.changeScoreBy(-1)
+})
+sprites.onOverlap(SpriteKind.ms2, SpriteKind.Enemy, function on_overlap4(sprite: Sprite, otherSprite: Sprite) {
+    sprites.destroy(sprite)
+    sprites.destroy(otherSprite)
+})
+game.onUpdateInterval(10000, function on_update_interval() {
+    let enemy = sprites.create(assets.image`enemy`, SpriteKind.Enemy)
+    enemy.setPosition(randint(0, 160), randint(0, 120))
+    enemy.follow(my_sprite, 20)
+})
+game.onUpdateInterval(5000, function on_update_interval2() {
+    let spr = sprites.create(assets.image`plate`, SpriteKind.z)
+    spr.setPosition(randint(0, 160), randint(0, 120))
+    spr.setScale(.5, ScaleAnchor.Middle)
 })
